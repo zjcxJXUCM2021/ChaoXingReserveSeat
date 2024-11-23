603bdbe5e3d711046ad5344d032163128e9354d6
zjcxJXUCM2021 <107027946+zjcxJXUCM2021@users.noreply.github.com>
2024-11-24 00:59:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ ENDTIME = "01:00:01" # 根据学校的预约座位时间+1min即可
 
 ENABLE_SLIDER = False # 是否有滑块验证
 MAX_ATTEMPT = 50 # 最大尝试次数
-RESERVE_NEXT_DAY = True # 预约明天而不是今天的
+RESERVE_NEXT_DAY = False # 预约明天而不是今天的
 
                 
 
